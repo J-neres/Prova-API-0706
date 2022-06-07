5750099373eec1be5e37388a619fb3bab5098a14
Juliano Neres <neres.julianosouzacosta@gmail.com>
2022-06-07 09:48:29 -0300

    commit

--- src/endpoints/funenpoints.js
+++ src/endpoints/funenpoints.js
@@ -3,8 +3,32 @@ import { InserirSuperHeroi, buscarPorNome } from "../services/funcoes";
 import {Router} from 'express';
 const server = Router();
 
-server.post('/pet, async (req, resp') => {
+server.post('/pet', async (req, resp) => {
     try{
-        const = novoheroi = req.body
+        const = novoheroi = req.body;
+        if(!nomeheroi.nome)
+            throw new error ('Nome do herói é obrigatório');
+
+            const heroiInserido = await InserirSuperHeroi(novoheroi)
+            resp.send(heroiInserido);
+    }catch(err) {
+        resp.status(401).send({
+            erro:err.message
+        });
     }
-}
+})
+
+server.get ('/heroi/busca', async (req, resp) =>{
+    try {
+        const {nome} = req.query;
+        const resposta = await buscarPorNome(nome);
+        if (!resposta)
+            resp.status(404).send([])
+        else 
+            resp.send (repsota);
+    }catch (err) {
+        resp.status(400).send({
+            erro:err.message
+        });
+    }
+})
--- src/services/funcoes.js
+++ src/services/funcoes.js
@@ -1,5 +1,6 @@
 import Connection from "mysql2/typings/mysql/lib/Connection";
 import {con} from './connection.js'
+
 export async function InserirSuperHeroi (heroi){
     const comando = 'insert into tb_super_heroi (id_super_heroi, nm_super_heroi, ds_super_heroi, bl_voa)
         values (?,?,?,?)'
@@ -10,13 +11,8 @@ export async function InserirSuperHeroi (heroi){
     return heroi;
 }
 
-export async function buscarPorNome  (nome) {
-    const comando = 'select id_heroi,
-                        nm_heroi,
-                        ds_heroi,
-                        bl_voa
-                    from tb_superheroi
-                    where nm_heroi like ?';
-const [linhas] = await con.query (comando, ['%${nome}%']);
+export async function buscarPorNome (nome) {
+    const comando = 'select id_heroi, nm_heroi, ds_heroi, bl_voa from tb_superheroi where nm_heroi like ?';
+    const [linhas] = await con.query (comando, ['%${nome}%']);
 return linhas[0]; 
 }
